412f94e73831475e76b4d16943e3b50c49b874d3
LAISHAWJ <lw2021-0468@uce.edu.do>
2026-04-12 14:54:06 -0400

    Agregar archivos de proyecto.

---
+++ ProyectMobilBD/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProyectMobilBD
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
