c2c8efef3cecd933e544aa78b5b6e4ddae8b9603
LAISHAWJ <lw2021-0468@uce.edu.do>
2026-04-12 15:30:06 -0400

    La clase anterior de ConexionDb era para crear la ruta de la base de datos. Esta clase de EmpleadoDbContext se creo la configuracion de la base de datos

---
+++ ProyectMobilBD/DataAccess/EmpleadoDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ProyectMobilBD.Models;
+using ProyectMobilBD.Utilidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectMobilBD.DataAccess
+{
+    public class EmpleadoDbContext : DbContext
+    {
+        public DbSet<Empleados> Empleados { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conexionDB = $"Filename={ConexionDB.DevolverRuta("empleados.db")}";
+            optionsBuilder.UseSqlite(conexionDB);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Empleados>(entity =>
+
+            {
+                entity.HasKey(e => e.IdEmpleado);
+                entity.Property(e => e.IdEmpleado).IsRequired().ValueGeneratedOnAdd();
+
+            });
+        }
+
+    }
+}
