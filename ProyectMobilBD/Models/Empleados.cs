32a3423d4e9067279accfc1d9ee686340a62eb6e
LAISHAWJ <lw2021-0468@uce.edu.do>
2026-04-12 15:05:31 -0400

    Creacion de carpeta de modelos para crear la clase de Empleados con sus propiedades

---
+++ ProyectMobilBD/Models/Empleados.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectMobilBD.Models
+{
+    public class Empleados
+    {
+        [Key]
+        public int IdEmpleado { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+        public decimal Sueldo { get; set; }
+        public DateTime FechaContrato { get; set; }
+    }
+}
