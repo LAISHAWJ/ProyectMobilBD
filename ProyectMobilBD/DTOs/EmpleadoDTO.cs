559e30946e5ccdd9a335eaf0da42c2a7ff5c930b
LAISHAWJ <lw2021-0468@uce.edu.do>
2026-04-12 15:52:41 -0400

    Creacion de carptea DTOs donde se creo la clase para hacer referencia a la clase modelo para no trabajar con la misma clase directamente de modelo.

---
+++ ProyectMobilBD/DTOs/EmpleadoDTO.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ProyectMobilBD.DTOs
+{
+    public partial class EmpleadoDTO : ObservableObject
+    {
+        [ObservableProperty]
+        public int idEmpleado;
+
+        [ObservableProperty]
+        public string nombreCompleto;
+
+        [ObservableProperty]
+        public string correo;
+
+        [ObservableProperty]
+        public decimal sueldo;
+
+        [ObservableProperty]
+        public DateTime fechaContrato; 
+    }
+}
